d7a13ef3a1b8d34f5166fa1ead4b3e4f730735e4
Vineet Parekh <pvineet44@gmail.com>
2022-10-28 17:12:26 +0100

    bot

---
+++ bot/src/commands/chat/help-command.ts
@@ -0,0 +1,38 @@
+import { ChatInputCommandInteraction, EmbedBuilder, PermissionsString } from 'discord.js';
+
+import { HelpOption } from '../../enums/index.js';
+import { Language } from '../../models/enum-helpers/index.js';
+import { EventData } from '../../models/internal-models.js';
+import { Lang } from '../../services/index.js';
+import { InteractionUtils } from '../../utils/index.js';
+import { Command, CommandDeferType } from '../index.js';
+
+export class HelpCommand implements Command {
+    public names = [Lang.getRef('chatCommands.help', Language.Default)];
+    public deferType = CommandDeferType.PUBLIC;
+    public requireClientPerms: PermissionsString[] = [];
+    public async execute(intr: ChatInputCommandInteraction, data: EventData): Promise<void> {
+        let option = intr.options.getString(Lang.getRef('arguments.option', Language.Default));
+
+        let embed: EmbedBuilder;
+        switch (option) {
+            case HelpOption.COMMANDS: {
+                embed = Lang.getEmbed('displayEmbeds.commands', data.lang);
+                break;
+            }
+            case HelpOption.PERMISSIONS: {
+                embed = Lang.getEmbed('displayEmbeds.permissions', data.lang);
+                break;
+            }
+            case HelpOption.FAQ: {
+                embed = Lang.getEmbed('displayEmbeds.faq', data.lang);
+                break;
+            }
+            default: {
+                return;
+            }
+        }
+
+        await InteractionUtils.send(intr, embed);
+    }
+}
