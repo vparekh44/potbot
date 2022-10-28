bce6b3ccb911719c1b8ae84514e63c25765a5048
Dmytro Izotov <izotov.dima@gmail.com>
2022-10-28 18:32:10 +0100

    Add theming

--- next/pages/index.tsx
+++ next/pages/index.tsx
@@ -1,17 +1,15 @@
-import Head from 'next/head';
-import Image from 'next/image';
-import { useEffect } from 'react';
-import { supabase } from '../lib/supabaseClient';
-import styles from '../styles/Home.module.css';
+import { useTheme } from "next-themes";
+
 
 export default function Home() {
+  const { theme, setTheme } = useTheme()
 
-
+  
   return (
-    <div className='flex items-center justify-center h-screen flex-col gap-4'>
-   <h1 className='text-slate-900 text-5xl'>HELLO FROM TAILWIND</h1>
-   <button className="btn btn-active btn-primary">Hello from daisyUi</button>
-   </div>
-
+    <div className="flex items-center justify-center h-screen flex-col gap-4">
+      <h1 className="text-primary text-5xl">HELLO FROM TAILWIND</h1>
+      <button className="btn btn-active btn-secondary" onClick={() => setTheme('dark')}>Dark Mode</button>
+      <button className="btn btn-active btn-secondary" onClick={() => setTheme('light')}>Light Mode</button>
+    </div>
   );
 }
--- next/tailwind.config.js
+++ next/tailwind.config.js
@@ -1,6 +1,41 @@
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
+  daisyui: {
+    themes: [
+      ...[
+        "light",
+        "dark",
+        "cupcake",
+        "bumblebee",
+        "emerald",
+        "corporate",
+        "synthwave",
+        "retro",
+        "cyberpunk",
+        "valentine",
+        "halloween",
+        "garden",
+        "forest",
+        "aqua",
+        "lofi",
+        "pastel",
+        "fantasy",
+        "wireframe",
+        "black",
+        "luxury",
+        "dracula",
+        "cmyk",
+        "autumn",
+        "business",
+        "acid",
+        "lemonade",
+        "night",
+        "coffee",
+        "winter",
+      ],
+    ],
+  },
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
