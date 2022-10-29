33cb9b615021c85bc77b22d86f39338a0415e277
Dmytro Izotov <izotov.dima@gmail.com>
2022-10-29 11:46:41 +0100

    Sync bottom/top nav

--- next/components/BottomNavigation.tsx
+++ next/components/BottomNavigation.tsx
@@ -1,3 +1,4 @@
+import { useConnectModal } from "@web3modal/react";
 import classNames from "classnames";
 import { useRouter } from "next/router";
 import { CgHome, CgProfile, CgPoll, CgEnter } from "react-icons/cg";
@@ -6,24 +7,29 @@ import { useUserData } from "../contexts/AuthContext";
 export const BottomNavigation = () => {
   const router = useRouter();
   const user = useUserData();
+  const { open: openLoginModal } = useConnectModal();
 
   return (
     <div className="btm-nav w-full sm:invisible">
       <button
-        onClick={() => router.push("/settings")}
-        className={classNames({ "active": router.asPath === "" })}
+        onClick={() => router.push("/leaderboard")}
+        className={classNames({ active: router.asPath === "" })}
       >
         <CgPoll className="h-6 w-6" />
       </button>
       <button
         onClick={() => router.push("/")}
-        className={classNames({ "active": router.asPath === "/" })}
+        className={classNames({ active: router.asPath === "/" })}
       >
         <CgHome className="h-6 w-6" />
       </button>
       <button
-        onClick={() => {}}
-        className={classNames({ "active": router.asPath === "" })}
+        onClick={() => {
+          user
+            ? router.push(`/profile/${user.walletAddress}`)
+            : openLoginModal();
+        }}
+        className={classNames({ active: router.asPath === "" })}
       >
         {user ? (
           <CgProfile className="h-6 w-6" />
--- next/components/TopNavigation.tsx
+++ next/components/TopNavigation.tsx
@@ -1,14 +1,19 @@
 import { useAccount, useConnectModal } from "@web3modal/react";
+import classNames from "classnames";
 import { useTheme } from "next-themes";
 import Link from "next/link";
+import { useRouter } from "next/router";
+import { CgEnter, CgHome, CgPoll, CgProfile } from "react-icons/cg";
 import { TbMoon, TbSun } from "react-icons/tb";
-import { useLogout } from "../contexts/AuthContext";
+import { useLogout, useUserData } from "../contexts/AuthContext";
 
 export const TopNavigation = () => {
   const { theme, setTheme } = useTheme();
-  const { open } = useConnectModal();
+  const { open: openLoginModal } = useConnectModal();
   const logout = useLogout();
   const { account } = useAccount();
+  const router = useRouter();
+  const user = useUserData();
 
   const toggleTheme = () => {
     theme !== "dark" ? setTheme("dark") : setTheme("light");
@@ -29,15 +34,40 @@ export const TopNavigation = () => {
           POTBOT
         </Link>
       </div>
-      <div className="flex-none">
+      <div className="flex-none gap-2">
         <button
-          onClick={account.isConnected ? handleDisconnect : handleConnect}
-          className="btn btn-ghost normal-case text-xl"
+          onClick={() => router.push("/leaderboard")}
+          className={"btn gap-2"}
         >
-          {account.isConnected ? "Disconnect" : "Connect"}
+          <CgPoll className="h-6 w-6" />
+          Leaderboard
+        </button>
+        <button onClick={() => router.push("/")} className={"btn gap-2"}>
+          <CgHome className="h-6 w-6" />
+          Home
         </button>
         <button
-          className="btn btn-ghost normal-case text-xl"
+          onClick={() => {
+            user
+              ? router.push(`/profile/${user.walletAddress}`)
+              : openLoginModal();
+          }}
+          className={"btn gap-2"}
+        >
+          {user ? (
+            <>
+              <CgProfile className="h-6 w-6" />
+              Profile
+            </>
+          ) : (
+            <>
+              <CgEnter className="h-6 w-6" />
+              Login
+            </>
+          )}
+        </button>
+        <button
+          className={"btn gap-2"}
           onClick={toggleTheme}
         >
           {theme !== "dark" ? <TbMoon size={24} /> : <TbSun size={24} />}
