e630979dcc2533f9601bbed6a594c20f9f56b19f
Vineet Parekh <pvineet44@gmail.com>
2022-10-29 15:41:09 +0100

    stats

--- next/components/Stats.tsx
+++ next/components/Stats.tsx
@@ -1,26 +1,32 @@
-<h2 className="text-4xl py-10 font-extrabold">Global Stats</h2>
-<progress
-  className="progress progress-primary w-56"
-  value="0"
-  max="100"
-></progress>
-<progress
-  className="progress progress-primary w-56"
-  value="10"
-  max="100"
-></progress>
-<progress
-  className="progress progress-primary w-56"
-  value="40"
-  max="100"
-></progress>
-<progress
-  className="progress progress-primary w-56"
-  value="70"
-  max="100"
-></progress>
-<progress
-  className="progress progress-primary w-56"
-  value="100"
-  max="100"
-></progress>
+const Stats = () => {
+  return (
+    <><h2 className="text-4xl py-10 font-extrabold">Global Stats</h2>
+      <progress
+        className="progress progress-primary w-56"
+        value="0"
+        max="100"
+      ></progress>
+      <progress
+        className="progress progress-primary w-56"
+        value="10"
+        max="100"
+      ></progress>
+      <progress
+        className="progress progress-primary w-56"
+        value="40"
+        max="100"
+      ></progress>
+      <progress
+        className="progress progress-primary w-56"
+        value="70"
+        max="100"
+      ></progress>
+      <progress
+        className="progress progress-primary w-56"
+        value="100"
+        max="100"
+      ></progress></>
+  );
+};
+
+export default Stats;
