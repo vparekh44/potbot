64b94a49f3847789c30aa9266b6d58f8af5d0c7f
Vineet Parekh <pvineet44@gmail.com>
2022-10-29 16:02:30 +0100

    change

--- next/components/AddPotbotToServer.tsx
+++ next/components/AddPotbotToServer.tsx
@@ -1,7 +1,6 @@
 import Link from "next/link";
 
 const AddPotBotToServer = () => {
-
   const ADD_DISCORD_URL =
     "https://discord.com/api/oauth2/authorize?client_id=1035598715634843809&permissions=8&redirect_uri=https%3A%2F%2Fpotbot.club%2F&response_type=code&scope=identify%20connections%20guilds%20guilds.join%20guilds.members.read%20bot%20applications.commands%20messages.read";
 
